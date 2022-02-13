ab5ab4d7d224951fae988057a1d0c45681ea935b
Su-Hyun Park <lovehyun@hotmail.com>
2022-02-14 02:28:09 +0900

    [NEW] convert to pytest

--- test_hello.py
+++ test_hello.py
@@ -1,13 +1,13 @@
+import unittest
 import hello
 
-class TestHello():
-  def test_add(self):
-    instance = hello.Hello()
-    if instance.add(2,3) != 5:
-        print('FAILED')
-        exit(1)
-    print('PASS')
+
+class TestHello(unittest.TestCase):
+    def test_add(self):
+        instance = hello.Hello()
+        result = instance.add(2, 3)
+        self.assertEqual(result, 5)
+
 
 if __name__ == "__main__":
-    test = TestHello()
-    test.test_add()
+    unittest.main()
